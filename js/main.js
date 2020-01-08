2685be06e21783a27e409ff30ad78631566cdfc8
Mattias Mucherie <mattias.mattias.m@gmail.com>
2020-01-08 15:12:35 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+﻿var x = 0;
+var y = 0;
+
+$(document).ready(function () {
+    var spielbrett = document.getElementById('leinwand');
+    spielfeld = spielbrett.getContext('2d');
+    var spielfigur = new Image();
+    spielfigur.src = 'images/spielfigur.png';
+
+    spielfigur.onload = function () {
+        spielfeld.drawImage(spielfigur, x, y);
+    }
+});
